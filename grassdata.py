7a7c8677ae1911ceff9b2e5404be4e63db28d64c
Stevenkerman27 <zyx20031020@gmail.com>
2026-03-31 20:02:12 +0100

    chore

--- grassdata.py
+++ grassdata.py
@@ -27,18 +27,18 @@ class Tree(object):
         def is_sym(self):
             return self.node_type == Tree.NodeType.SYM
 
-    def __init__(self, boxes, ops, syms):
-        box_list = [b for b in torch.split(boxes, 1, 0)]
+    def __init__(self, boxes, ops, syms): #boxes: torch vector [M,12], syms torch vector [N,8]
+        box_list = [b for b in torch.split(boxes, 1, 0)] #切成M个[1, 12]
         sym_param = [s for s in torch.split(syms, 1, 0)]
         box_list.reverse()
         sym_param.reverse()
         queue = []
         for id in range(ops.size()[1]):
             if ops[0, id] == Tree.NodeType.BOX.value:
-                queue.append(Tree.Node(box=box_list.pop(), node_type=Tree.NodeType.BOX))
+                queue.append(Tree.Node(box=box_list.pop(), node_type=Tree.NodeType.BOX)) #读到box，把box添加入栈
             elif ops[0, id] == Tree.NodeType.ADJ.value:
                 left_node = queue.pop()
-                right_node = queue.pop()
+                right_node = queue.pop() #跳出两个node,放入一个ADJ node
                 queue.append(Tree.Node(left=left_node, right=right_node, node_type=Tree.NodeType.ADJ))
             elif ops[0, id] == Tree.NodeType.SYM.value:
                 node = queue.pop()
@@ -54,9 +54,9 @@ class GRASSDataset(data.Dataset):
         op_data = torch.from_numpy(loadmat(self.dir+'/op_data.mat')['ops']).int()
         sym_data = torch.from_numpy(loadmat(self.dir+'/sym_data.mat')['syms']).float()
         #weight_list = torch.from_numpy(loadmat(self.dir+'/weights.mat')['weights']).float()
-        num_examples = op_data.size()[1]
-        box_data = torch.chunk(box_data, num_examples, 1)
-        op_data = torch.chunk(op_data, num_examples, 1)
+        num_examples = op_data.size()[1] #样本数
+        box_data = torch.chunk(box_data, num_examples, 1) #切成num_example个独立tensor
+        op_data = torch.chunk(op_data, num_examples, 1) #读取正确的合并顺序
         sym_data = torch.chunk(sym_data, num_examples, 1)
         #weight_list = torch.chunk(weight_list, num_examples, 1)
         self.transform = transform
